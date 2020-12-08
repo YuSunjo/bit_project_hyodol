5074294e354b3a9aae20b68852b4161c2e830398
yusunjo <tnswh2023@gmail.com>
2020-12-08 10:49:12 +0900

    nunjucks 사용

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 
 nunjucks.configure('template', {
+    //ture -> message 안에 html 태그가 안들어감 들어가게 하려면 {{message | safe}} 처럼 뒤에 safe넣으면 된다.
     autoescape : true, 
     express : app
 });
