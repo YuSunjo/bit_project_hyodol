10053cc44da7299c2220478d6c367589bdd4353d
yusunjo <tnswh2023@gmail.com>
2020-12-08 10:17:31 +0900

    template engine 설정

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const customer = require('./routes/customer');
 
 const app = express();
 const port = 3000;
 
+nunjucks.configure('template', {
+    autoescape : true, 
+    express : app
+});
+
 app.get('/', (req, res) => {
-    res.send('hello');
+    // res.send('hello');
+    //template 파일 이후부터 지정해주면 된다. 
+    res.render( 'customer/customer.html', {
+        message : 'hello'
+    })
 })
 
 app.use('/cutomer', customer);
